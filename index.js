398e97b7c5865541ebb8749a4ec520ed992dc6cc
Fred Chien <fred@mandice.com>
2012-03-14 19:25:37 +0800

    Initial version

---
+++ index.js
@@ -0,0 +1,34 @@
+//var dbus = module.exports = require('node-dbus');
+var dbus = require('node-dbus');
+
+module.exports = function() {
+	var self = this;
+
+	this.init = function(callback) {
+		dbus.start(function() {
+			self.systemBus = dbus.system_bus();
+			self.manager = dbus.get_interface(self.systemBus, 'net.connman', '/', 'net.connman.Manager');
+			self.properties = self.manager.GetProperties();
+
+			console.log(self.manager);
+
+			callback();
+		});
+	};
+
+	this.__defineGetter__('AvailableTechnologies', function() {
+		return self.properties.AvailableTechnologies;
+	});
+
+	this.__defineGetter__('GetState', function() {
+		return self.manager.GetState();
+	});
+
+	this.__defineGetter__('OfflineMode', function() {
+		return self.manager.GetProperties().OfflineMode;
+	});
+
+	this.__defineSetter__('OfflineMode', function(value) {
+		return self.manager.SetProperty('OfflineMode', false);
+	});
+};

2644267a597501025c06b02fce5a9ee30c5117f5
Fred Chien <fred@mandice.com>
2012-03-24 00:42:25 +0800

    Use Agent to connect wifi access point

--- tests/ConnectAP.js
+++ tests/ConnectAP.js
@@ -2,5 +2,22 @@ var ConnMan = require('../index.js');
 
 var connman = new ConnMan();
 connman.init(function() {
+
+	/* Initializing Agent for connectiing access point */
+	connman.Agent.on('RequestInput', function(path, dict) {
+		console.log(dict);
+
+		if ('Passphrase' in dict) {
+			return { 'Passphrase': '11111' };
+		}
+	});
+
+	connman.Agent.on('Cancel', function() {
+		console.log('Cancel');
+	});
+
+	connman.Agent.run();
+
+	/* Making connection with access point which is called '1F' */
 	connman.Wifi.Connect('1F');
 });

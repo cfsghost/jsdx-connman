a10b4803d2871ff08a7e541b26c0766459d1fcd0
Fred Chien <fred@mandice.com>
2012-09-22 16:25:53 +0800

    pump version

--- tests/ConnectAP.js
+++ tests/ConnectAP.js
@@ -10,6 +10,7 @@ connman.init(function() {
 	connman.Agent.on('ReportError', function(path, err) {
 		console.log('ReportError:');
 		console.log(err);
+		/* connect-failed */
 		/* invalid-key */
 	});
 
@@ -20,13 +21,24 @@ connman.init(function() {
 	/* Initializing Agent for connectiing access point */
 	connman.Agent.on('RequestInput', function(path, dict) {
 		console.log(dict);
+//		return { 'Passphrase': 'CCMA0275' };
+
+		/* leaf */
+//		return { 'Passphrase': '23634943' };
+
+		/* Peixin */
+//		return { 'Passphrase': '12345' };
+//		return { 'Passphrase': '882525270' };
 
 		/* dCafe */
 //		return { 'Passphrase': '0223919233' };
-//		return { 'Passphrase': '882525270' };
+//		return { 'Passphrase': '23919233' };
 
 		/* Symbio */
 //		return { 'Passphrase': 'abbab2717398327173983abbab' };
+
+		/* Symbio TaipeiG */
+		return { 'Passphrase': 'abbab2717398327174283abbab' };
 //
 		/* GKtea */
 		//return { 'Passphrase': '9876543210' };
@@ -57,20 +69,26 @@ connman.init(function() {
 		console.log('Cancel');
 	});
 
-	connman.Agent.run();
-
 	/* Making connection with access point which is called '1F' */
 	//connman.Wifi.Connect('CPBAE(C.I.E)-WB08');
 	//connman.Wifi.Connect('trendmicro');
-	//connman.Wifi.Connect('培培的愛逢逢');
+//	connman.Wifi.Connect('培培的愛逢逢');
 //	connman.Wifi.Connect('dcafe');
+//	connman.Wifi.Connect('dcafe-2');
 //	connman.Wifi.Connect('SymbioGuest');
+	connman.Wifi.Connect('TaipeiG');
+//	connman.Wifi.Connect('Rebirth Cafe-4free');
+//	connman.Wifi.Connect('TPE-Free');
+//	connman.Wifi.Connect('CCMA');
 	//connman.Wifi.Connect('GKtea');
 	//connman.Wifi.Connect('NewTaipei');
 	//connman.Wifi.Connect('Bastille WLAN');
 //	connman.Wifi.Connect('ZyXEL-OSSII');
 //	connman.Wifi.Connect('insomnia');
 	//connman.Wifi.Connect('frog-cafe');
-	connman.Wifi.Connect('1F');
+//	connman.Wifi.Connect('1F');
+//	connman.Wifi.Connect('Connectify-Peixin');
+	//connman.Wifi.Connect('WBR-6011');
+//	connman.Wifi.Connect('leaf');
 	
 });

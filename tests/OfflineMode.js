3fd78f011fb455aa3eec58fcd0206cec8e6f8d70
Fred Chien <fred@mandice.com>
2013-09-30 22:07:39 +0800

    modified testcase for offline mode

--- tests/OfflineMode.js
+++ tests/OfflineMode.js
@@ -2,7 +2,7 @@ var ConnMan = require('../index.js');
 
 var connman = new ConnMan();
 connman.init(function() {
-	console.log(connman.OfflineMode);
-	connman.OfflineMode = false;
-	console.log(connman.OfflineMode);
+	connman.setOfflineMode(true, function() {
+		process.exit();
+	});
 });

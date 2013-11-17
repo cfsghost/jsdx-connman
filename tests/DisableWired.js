0d378a451827fc9932fb248bd5484026c4fa59bc
Fred Chien <cfsghost@gmail.com>
2013-11-17 21:33:55 +0800

    support multi wired device

--- tests/DisableWired.js
+++ tests/DisableWired.js
@@ -3,9 +3,22 @@ var ConnMan = require('../');
 var connman = new ConnMan();
 connman.init(function() {
 
-	connman.Wired.disconnect(function(err) {
-		connman.Wired.setProperty('Powered', false, function() {
-			process.exit();
-		});
+	connman.getServices('wired', function(err, services) {
+
+		for (var serviceName in services) {
+
+			connman.getConnection(serviceName, function(err, conn) {
+
+				// Disconnection
+				conn.disconnect(function(err) {
+
+					// Power off
+					conn.setProperty('Powered', false, function() {
+
+					});
+				});
+				
+			});
+		}
 	});
 });
--- tests/EnableWired.js
+++ tests/EnableWired.js
@@ -3,11 +3,22 @@ var ConnMan = require('../');
 var connman = new ConnMan();
 connman.init(function() {
 
-	connman.Wired.setProperty('Powered', true, function() {
+	connman.getServices('wired', function(err, services) {
 
-		connman.Wired.connect(function(err) {
-			console.log('Enabled');
-			process.exit();
-		});
+		for (var serviceName in services) {
+
+			connman.getConnection(serviceName, function(err, conn) {
+
+				// Powered
+				conn.setProperty('Powered', true, function() {
+
+					// Establish a connection
+					conn.connect(function(err) {
+
+					});
+				});
+				
+			});
+		}
 	});
 });

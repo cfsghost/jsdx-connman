3961fa4d4e7605af459cd76bfa6b8ef63d6766ef
Fred Chien <cfsghost@gmail.com>
2013-11-19 01:31:19 +0800

    add bluetooth

--- tests/EnableBluetooth.js
+++ tests/EnableBluetooth.js
@@ -1,11 +1,31 @@
 var ConnMan = require('../');
+var async = require('async');
 
 var connman = new ConnMan();
 connman.init(function() {
 
-	connman.Bluetooth.setProperty('Powered', true, function() {
+	var bluetooth = connman.technologies['Bluetooth'];
 
-		console.log('Enabled');
-		process.exit();
+	// Powered
+	bluetooth.setProperty('Powered', true, function() {
+
+		// Getting available connections
+		bluetooth.getServices(function(err, services) {
+
+			async.eachSeries(Object.keys(services), function(serviceName, next) {
+
+				connman.getConnection(serviceName, function(err, conn) {
+
+					// Establish a connection
+					conn.connect(function(err) {
+						next();
+					});
+					
+				});
+			}, function() {
+				console.log('Enabled');
+				process.exit();
+			});
+		});
 	});
 });

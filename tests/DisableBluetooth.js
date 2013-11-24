b6f327cf38e0afefe17657615696c5956feb1061
Fred Chien <cfsghost@gmail.com>
2013-11-24 20:30:43 +0800

    add testcase that disable bluetooth

---
+++ tests/DisableBluetooth.js
@@ -0,0 +1,36 @@
+var ConnMan = require('../');
+var async = require('async');
+
+var connman = new ConnMan();
+connman.init(function() {
+
+	var bluetooth = connman.technologies['Bluetooth'];
+
+	// Getting current connections
+	bluetooth.getServices(function(err, services) {
+
+		async.eachSeries(Object.keys(services), function(serviceName, next) {
+
+			connman.getConnection(serviceName, function(err, conn) {
+
+				// Disconnect
+				conn.disconnect(function(err) {
+
+					next();
+
+				});
+				
+			});
+
+		}, function() {
+
+			// Power off
+			bluetooth.setProperty('Powered', false, function() {
+
+				console.log('Disabled');
+				process.exit();
+
+			});
+		});
+	});
+});

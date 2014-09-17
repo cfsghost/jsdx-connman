b4c7cc5fc68e4627270d7848878c44d46efbef59
Arne Boon <arneboon@gmail.com>
2014-09-17 10:59:16 +0200

    add support for tethering a technology: enable and disable. including test examples

---
+++ tests/EnableTethering.js
@@ -0,0 +1,22 @@
+//NOTE: Network.js DBUS needs environment vars below
+//from: http://stackoverflow.com/questions/8556777/dbus-php-unable-to-launch-dbus-daemon-without-display-for-x11
+process.env.DISPLAY = ':0';
+process.env.DBUS_SESSION_BUS_ADDRESS = 'unix:path=/run/dbus/system_bus_socket';
+
+var ConnMan = require('../');
+
+var connman = new ConnMan();
+
+connman.init(function() {
+
+	var wifi = connman.technologies['WiFi'];
+
+	wifi.enableTethering('HotspotSSID', 'password123', function(err, res) {
+		if (!err) {
+			console.log('Tethering enabled');
+		} else {
+			console.log(err);
+		}
+	});
+
+});
